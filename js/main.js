9a553cdd75f156e8c73b23a67d60f9258d4c8dea
Roman <romas6ka@i.ua>
2018-12-13 21:40:25 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,10 @@
-// var slideIndex = 1;
-// showSlides(slideIndex);
+$(document).ready(function(){
+  	$('ul li a').click(function(){
+    	$('li a').removeClass("active");
+    	$(this).addClass("active");
+	});
+});
 
-// function plusSlides(n) {
-//   	showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   	showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-// 	var i;
-// 	var slides = (".mySlides");
-// 	var dots = (".dot");
-// 	if (n > slides.length) {slideIndex = 1}
-// 	if (n < 1) {slideIndex = slides.length}
-// 	for (i = 0; i < slides.length; i++) {
-//     	slides[i].style.display = "none";
-//   	}
-//   	for (i = 0; i < dots.length; i++) {
-//       	dots[i].className = dots[i].className.replace(" active", "");
-//   	}
-//   	slides[slideIndex-1].style.display = "block";
-//   	dots[slideIndex-1].className += " active";
-// }
-
-
-
-$('.carousel').carousel({
-  interval: 1000
-})
+// $('.carousel').carousel({
+//   interval: 1000
+// })
