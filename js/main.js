84624f4eb0e3219f2675c56a5f4808ba9683ba74
Roman <romas6ka@i.ua>
2018-12-15 11:29:30 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,81 @@ $(document).ready(function(){
 	});
 });
 
-// $('.carousel').carousel({
-//   interval: 1000
-// })
+$('#bannerSlide').carousel({
+  	interval: 2000
+});
+
+$(document).ready(function(){
+    $('.project-2').trigger('hover');
+    $('.project-2').trigger('mouseenter');
+    $('.project-2').trigger('mouseover');
+});
+
+$(document).ready(function(){
+  	$(".project-1").hover(function(){
+    	$(".ellipse-1").css("display", "block");
+    	}, function(){
+    	$(".ellipse-1").css("display", "none");
+  	});
+  	$(".ellipse-1").hover(function(){
+    	$(this).css("display", "block");
+    	});
+  	$(".project-2").hover(function(){
+    	$(".ellipse-2").css("display", "block");
+    	}, function(){
+    	$(".ellipse-2").css("display", "none");
+  	});
+  	$(".project-3").hover(function(){
+    	$(".ellipse-3").css("display", "block");
+    	}, function(){
+    	$(".ellipse-3").css("display", "none");
+  	});
+  	$(".project-4").hover(function(){
+    	$(".ellipse-4").css("display", "block");
+    	}, function(){
+    	$(".ellipse-4").css("display", "none");
+  	});
+});
+
+
+// $('#carousel').on('slide.bs.carousel', function (e) {
+
+  
+//     var $e = $(e.relatedTarget);
+//     var idx = $e.index();
+//     var itemsPerSlide = 4;
+//     var totalItems = $('.carousel-item').length;
+    
+//     if (idx >= totalItems-(itemsPerSlide-1)) {
+//         var it = itemsPerSlide - (totalItems - idx);
+//         for (var i=0; i<it; i++) {
+//             // append slides to end
+//             if (e.direction=="left") {
+//                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
+//             }
+//             else {
+//                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
+//             }
+//         }
+//     }
+// });
+
+
+//   $('#carouselExample').carousel({ 
+//                 interval: 2000
+//         });
+
+
+//   $(document).ready(function() {
+// /* show lightbox when clicking a thumbnail */
+//     $('a.thumb').click(function(event){
+//       event.preventDefault();
+//       var content = $('.modal-body');
+//       content.empty();
+//         var title = $(this).attr("title");
+//         $('.modal-title').html(title);
+//         content.html($(this).html());
+//         $(".modal-profile").modal({show:true});
+//     });
+
+//   });
